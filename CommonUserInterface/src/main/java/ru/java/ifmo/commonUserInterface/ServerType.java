629d9853a0617efa6ccdb27720cec554cd645224
Anton Riabushev <ryabushev96@gmail.com>
2020-05-04 20:29:19 +0300

    add enums

---
+++ CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/ServerType.java
@@ -0,0 +1,7 @@
+package ru.java.ifmo.commonUserInterface;
+
+public enum ServerType {
+    INDIVIDUAL_THREAD_SERVER,
+    BLOCKING_SERVER,
+    NOT_BLOCKING_SERVER
+}
---
+++ CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/TypeOfVariableParameter.java
@@ -0,0 +1,7 @@
+package ru.java.ifmo.commonUserInterface;
+
+public enum TypeOfVariableParameter {
+    SIZE_OF_REQUEST,
+    NUMBER_OF_CLIENTS,
+    CLIENT_SLEEP_TIME
+}
