da0c234e1f0cc5f2163ac3135ce276c9ec84f8ef
Anton Riabushev <ryabushev96@gmail.com>
2020-05-04 21:33:36 +0300

    add some constant

---
+++ Common/src/main/java/ru/ifmo/java/common/Constant.java
@@ -0,0 +1,9 @@
+package ru.ifmo.java.common;
+
+public class Constant {
+    public static final String serverHost = "localhost";
+//    public static final String serverHost = "18.191.106.182";
+
+    public static final int managingServerPort = 8081;
+    public static final int computeServerPort = 8082;
+}
