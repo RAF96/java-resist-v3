664df6b261e5680635060acfe7dfac3731ae63cb
Anton Riabushev <ryabushev96@gmail.com>
2020-05-04 20:14:08 +0300

    develop complex testing settings

--- CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/SettingsOfComplexTestingOfServerPerformance.java
+++ CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/SettingsOfComplexTestingOfServerPerformance.java
@@ -1,6 +1,73 @@
 package ru.java.ifmo.commonUserInterface;
 
-//TODO
+import java.util.Iterator;
+import java.util.List;
+
 public interface SettingsOfComplexTestingOfServerPerformance extends Iterable<SettingsOfServerPerformanceTesting> {
+    static SettingsOfComplexTestingOfServerPerformance create(
+            List<Integer> rangeOfVariableParameter,
+            TypeOfVariableParameter typeOfVariableParameter,
+            ServerType serverType,
+            int numberOfClients,
+            int sizeOfRequest,
+            int numberOfRequestPerClient,
+            int clientSleepTime
+    ) {
+        return new SettingsOfComplexTestingOfServerPerformance() {
+            @Override
+            public Iterator<SettingsOfServerPerformanceTesting> iterator() {
+                return new Iterator<>() {
+                    int rangeIndex = 0;
+                    int sizeOfRequest;
+                    int numberOfClients;
+                    int clientSleepTime;
+
+                    {
+                        this.sizeOfRequest = sizeOfRequest;
+                        this.numberOfClients = numberOfClients;
+                        this.clientSleepTime = clientSleepTime;
+                    }
+
+                    @Override
+                    public boolean hasNext() {
+                        return rangeIndex == rangeOfVariableParameter.size();
+                    }
+
+                    @Override
+                    public SettingsOfServerPerformanceTesting next() {
+                        foo();
+                        SettingsOfServerPerformanceTesting settings = SettingsOfServerPerformanceTesting.create(
+                                serverType,
+                                numberOfClients,
+                                sizeOfRequest,
+                                numberOfRequestPerClient,
+                                clientSleepTime
+                        );
+                        rangeIndex += 1;
+                        return settings;
+                    }
+
+                    private void foo() {
+                        switch (typeOfVariableParameter) {
+                            case SIZE_OF_REQUEST:
+                                sizeOfRequest = rangeOfVariableParameter.get(rangeIndex);
+                            case CLIENT_SLEEP_TIME:
+                                clientSleepTime = rangeOfVariableParameter.get(rangeIndex);
+                                break;
+                            case NUMBER_OF_CLIENTS:
+                                numberOfClients = rangeOfVariableParameter.get(rangeIndex);
+                                break;
+                            default:
+                                throw new UnknownTypeOfVariableParameter();
+                        }
+                    }
+
+                    class UnknownTypeOfVariableParameter extends RuntimeException {
+                    }
+
+                };
+            }
+        };
+    }
 
 }
--- CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/SettingsOfServerPerformanceTesting.java
+++ CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/SettingsOfServerPerformanceTesting.java
@@ -1,6 +1,50 @@
 package ru.java.ifmo.commonUserInterface;
 
-//TODO
 public interface SettingsOfServerPerformanceTesting {
+    static SettingsOfServerPerformanceTesting create(
+            ServerType serverType,
+            int numberOfClients,
+            int sizeOfRequest,
+            int numberOfRequestPerClient,
+            int clientSleepTime
+    ) {
+        return new SettingsOfServerPerformanceTesting() {
+            @Override
+            public ServerType getServerType() {
+                return serverType;
+            }
 
+            @Override
+            public int getNumberOfClients() {
+                return numberOfClients;
+            }
+
+            @Override
+            public int getSizeOfRequest() {
+                return sizeOfRequest;
+            }
+
+            @Override
+            public int getNumberOfRequestPerClient() {
+                return numberOfRequestPerClient;
+            }
+
+            @Override
+            public int getClientSleepTime() {
+                return clientSleepTime;
+            }
+
+        };
+    }
+
+
+    ServerType getServerType();
+
+    int getNumberOfClients();
+
+    int getSizeOfRequest();
+
+    int getNumberOfRequestPerClient();
+
+    int getClientSleepTime();
 }
