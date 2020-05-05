af1ee6b203967c98980ca750dadb0c001154d825
Anton Riabushev <ryabushev96@gmail.com>
2020-05-05 13:59:11 +0300

    add transfer for ServerType

--- CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/CommonUserInterfaceImpl.java
+++ CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/CommonUserInterfaceImpl.java
@@ -1,31 +1,41 @@
 package ru.java.ifmo.commonUserInterface;
 
 import ru.ifmo.java.client.ClientMetrics;
+import ru.ifmo.java.common.Constant;
+import ru.ifmo.java.common.protocol.Protocol.*;
 
+
+import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommonUserInterfaceImpl implements CommonUserInterface {
+    private Socket managingServerSocket;
+
     @Override
     public AggregateServerPerformanceMetrics runComplexTestingOfServerPerformance(SettingsOfComplexTestingOfServerPerformance settingsOfComplexTesting) {
         List<ServerPerformanceMetrics> list = new ArrayList<>();
-        for (var settingsOfTesting : settingsOfComplexTesting) {
-            list.add(testServerPerformance(settingsOfTesting));
+        try {
+            for (var settingsOfTesting : settingsOfComplexTesting) {
+                list.add(testServerPerformance(settingsOfTesting));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return AggregateServerPerformanceMetrics.create(list);
     }
 
-    //TODO
-    private ServerPerformanceMetrics testServerPerformance(SettingsOfServerPerformanceTesting settings) {
+    private ServerPerformanceMetrics testServerPerformance(SettingsOfServerPerformanceTesting settings) throws IOException {
         runServer(settings);
         ClientMetrics clientMetrics = runClient(settings);
         ServerMetrics serverMetrics = getManagingServerResponse();
-        return null;
+        return ServerPerformanceMetrics.create(serverMetrics.getRequestProcessingTime(), serverMetrics.getClientProcessingTime(), clientMetrics.getAverageTimeSpendByClient());
     }
 
-    //TODO
-    private ServerMetrics getManagingServerResponse() {
-        return null;
+    private ServerMetrics getManagingServerResponse() throws IOException {
+        ResponseOfComputingServerStartup response = ResponseOfComputingServerStartup.parseDelimitedFrom(managingServerSocket.getInputStream());
+        return ServerMetrics.create(response.getRequestProcessingTime(), response.getClientProcessingTime());
     }
 
     //TODO
@@ -33,8 +43,11 @@ public class CommonUserInterfaceImpl implements CommonUserInterface {
         return null;
     }
 
-    //TODO
-    private void runServer(SettingsOfServerPerformanceTesting settings) {
-
+    private void runServer(SettingsOfServerPerformanceTesting settings) throws IOException {
+        managingServerSocket = new Socket(Constant.serverHost, Constant.managingServerPort);
+        RequestOfComputingServerStartup.newBuilder()
+                .setNumberOfClients(settings.getNumberOfClients())
+                .setServerType(ServerType.serverType2ProtocolServerType(settings.getServerType()))
+                .build().writeDelimitedTo(managingServerSocket.getOutputStream());
     }
 }
--- CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/ServerType.java
+++ CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/ServerType.java
@@ -1,7 +1,52 @@
 package ru.java.ifmo.commonUserInterface;
 
+import ru.ifmo.java.common.protocol.Protocol.*;
+
 public enum ServerType {
     INDIVIDUAL_THREAD_SERVER,
-    BLOCKING_SERVER,
-    NOT_BLOCKING_SERVER
+    BLOCKING_THREAD_SERVER,
+    NOT_BLOCKING_THREAD_SERVER;
+
+    public static ServerType protocolServerType2ServerType
+            (RequestOfComputingServerStartup.ServerType protocolServerType) {
+        ServerType serverType;
+        switch (protocolServerType) {
+            case BLOCKING_THREAD_SERVER:
+                serverType = BLOCKING_THREAD_SERVER;
+                break;
+            case INDIVIDUAL_THREAD_SERVER:
+                serverType = INDIVIDUAL_THREAD_SERVER;
+                break;
+            case NOT_BLOCKING_THREAD_SERVER:
+                serverType = NOT_BLOCKING_THREAD_SERVER;
+                break;
+            default:
+                throw new UnknownServerType();
+        }
+        return serverType;
+    }
+
+
+    public static RequestOfComputingServerStartup.ServerType
+            serverType2ProtocolServerType(ServerType serverType) {
+        RequestOfComputingServerStartup.ServerType protocolServerType;
+        switch (serverType) {
+            case BLOCKING_THREAD_SERVER:
+                protocolServerType = RequestOfComputingServerStartup.ServerType.BLOCKING_THREAD_SERVER;
+                break;
+            case INDIVIDUAL_THREAD_SERVER:
+                protocolServerType = RequestOfComputingServerStartup.ServerType.INDIVIDUAL_THREAD_SERVER;
+                break;
+            case NOT_BLOCKING_THREAD_SERVER:
+                protocolServerType = RequestOfComputingServerStartup.ServerType.NOT_BLOCKING_THREAD_SERVER;
+                break;
+            default:
+                throw new UnknownServerType();
+        }
+        return protocolServerType;
+    }
+
+
+    public static class UnknownServerType extends RuntimeException {
+    }
 }
