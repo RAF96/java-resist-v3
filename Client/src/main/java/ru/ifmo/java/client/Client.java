5e909e33989af98f58adcc509a03ee3349687d5e
Anton Riabushev <ryabushev96@gmail.com>
2020-05-06 18:47:05 +0300

    implement client
    Signed-off-by: Anton Riabushev <ryabushev96@gmail.com>

--- Client/src/main/java/ru/ifmo/java/client/Client.java
+++ Client/src/main/java/ru/ifmo/java/client/Client.java
@@ -1,11 +1,9 @@
 package ru.ifmo.java.client;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.atomic.AtomicReference;
 
-//TODO
 public interface Client extends Callable<ClientMetrics> {
     static Client create(ClientSettings clientSettings) {
-        return null;
+        return new ClientImpl(clientSettings);
     }
 }
---
+++ Client/src/main/java/ru/ifmo/java/client/ClientImpl.java
@@ -0,0 +1,75 @@
+package ru.ifmo.java.client;
+
+import ru.ifmo.java.common.Constant;
+import ru.ifmo.java.common.MessageProcessing;
+import ru.ifmo.java.common.protocol.Protocol.*;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
+
+public class ClientImpl implements Client {
+    private final ClientSettings clientSettings;
+    private final Random random = new Random();
+    private Socket socket;
+
+    public ClientImpl(ClientSettings clientSettings) {
+        assert clientSettings.getNumberOfRequest() > 0;
+        this.clientSettings = clientSettings;
+    }
+
+    @Override
+    public ClientMetrics call() throws Exception {
+        long startTimeMillis = System.currentTimeMillis();
+        socket = initSocket();
+        int numberOfSentRequest = 0;
+        try {
+            for (;numberOfSentRequest < clientSettings.getNumberOfRequest(); numberOfSentRequest++) {
+                processingOneRequest();
+            }
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+        long currentTimeMillis = System.currentTimeMillis();
+        sendHaltRequest2ManagingServer();
+        return ClientMetrics.create(numberOfSentRequest, currentTimeMillis - startTimeMillis);
+    }
+
+    private void sendHaltRequest2ManagingServer() throws IOException {
+        Socket socket = new Socket(Constant.serverHost, Constant.computeServerPort);
+        RequestOfHaltingOfComputingServer.newBuilder().build().writeDelimitedTo(socket.getOutputStream());
+    }
+
+
+    private Socket initSocket() throws InterruptedException {
+        boolean isSocketReady = false;
+        Socket socket = null;
+        do {
+            try {
+                socket = new Socket(Constant.serverHost, Constant.computeServerPort);
+                isSocketReady = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //FIXME. Magic constant
+            Thread.sleep(1000);
+
+        } while(!isSocketReady);
+        return socket;
+    }
+
+    private void processingOneRequest() throws IOException {
+        List<Double> list = random.doubles(clientSettings.getSizeOfRequest()).boxed().collect(Collectors.toList());
+        MessageWithListOfDoubleVariables message = MessageWithListOfDoubleVariables.newBuilder().addAllNumber(list).build();
+        byte[] packMessage = MessageProcessing.packMessage(message.toByteArray());
+        socket.getOutputStream().write(packMessage);
+        // waitResponse
+        byte[] responseInBytes = MessageProcessing.readPackedMessage(socket.getInputStream());
+        MessageWithListOfDoubleVariables response = MessageWithListOfDoubleVariables.parseFrom(responseInBytes);
+        Collections.sort(list);
+        assert Arrays.equals(response.getNumberList().toArray(), list.toArray());
+    }
+
+}
