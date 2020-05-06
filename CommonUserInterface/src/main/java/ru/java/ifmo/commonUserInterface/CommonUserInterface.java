66621683364d48b720528555fd6c8cd29c683062
Anton Riabushev <ryabushev96@gmail.com>
2020-05-06 13:36:41 +0300

    add one simple test

--- CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/CommonUserInterface.java
+++ CommonUserInterface/src/main/java/ru/java/ifmo/commonUserInterface/CommonUserInterface.java
@@ -1,6 +1,12 @@
 package ru.java.ifmo.commonUserInterface;
 
+import java.io.IOException;
+
 public interface CommonUserInterface {
+
+    static CommonUserInterface create() throws IOException {
+        return new CommonUserInterfaceImpl();
+    }
 
     AggregateServerPerformanceMetrics runComplexTestingOfServerPerformance(SettingsOfComplexTestingOfServerPerformance settings);
 }
---
+++ CommonUserInterface/src/test/java/ru/java/ifmo/commonUserInterface/CommonUserInterfaceTest.java
@@ -0,0 +1,27 @@
+package ru.java.ifmo.commonUserInterface;
+
+import org.junit.jupiter.api.Test;
+import ru.ifmo.java.common.ServerType;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+class CommonUserInterfaceTest {
+
+    @Test
+    void runComplexTestingOfServerPerformance() throws IOException {
+        CommonUserInterface commonUserInterface = CommonUserInterface.create();
+        SettingsOfComplexTestingOfServerPerformance settings = SettingsOfComplexTestingOfServerPerformance.create(
+                Collections.singletonList(0),
+                TypeOfVariableParameter.CLIENT_SLEEP_TIME,
+                ServerType.INDIVIDUAL_THREAD_SERVER,
+                2,
+                1000,
+                2,
+                0
+        );
+        AggregateServerPerformanceMetrics metrics = commonUserInterface.runComplexTestingOfServerPerformance(settings);
+    }
+}
