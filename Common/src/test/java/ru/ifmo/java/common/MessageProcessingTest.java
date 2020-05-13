756f76b6f8641a4a89637737ceda1a6ad208f50b
Anton Riabushev <ryabushev96@gmail.com>
2020-05-13 21:33:47 +0300

    add test
    Signed-off-by: Anton Riabushev <ryabushev96@gmail.com>

---
+++ Common/src/test/java/ru/ifmo/java/common/MessageProcessingTest.java
@@ -0,0 +1,13 @@
+package ru.ifmo.java.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageProcessingTest {
+
+    @Test
+    public void test() {
+
+    }
+}
