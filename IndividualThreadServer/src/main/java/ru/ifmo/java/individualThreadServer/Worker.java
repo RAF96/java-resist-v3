eb1439bba334d5253b8ec8917dc3ee9d3b818571
Anton Riabushev <ryabushev96@gmail.com>
2020-05-14 14:45:29 +0300

    small fixes
    Signed-off-by: Anton Riabushev <ryabushev96@gmail.com>

--- IndividualThreadServer/src/main/java/ru/ifmo/java/individualThreadServer/Worker.java
+++ IndividualThreadServer/src/main/java/ru/ifmo/java/individualThreadServer/Worker.java
@@ -3,11 +3,12 @@ package ru.ifmo.java.individualThreadServer;
 import ru.ifmo.java.commonPartsOfComputeServer.AverageServerMetrics;
 import ru.ifmo.java.commonPartsOfComputeServer.ComputeServerSettings;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.CountDownLatch;
 
 public interface Worker extends Runnable {
-    static Worker create(Socket socket, CountDownLatch latch, ComputeServerSettings computeServerSettings) {
+    static Worker create(Socket socket, CountDownLatch latch, ComputeServerSettings computeServerSettings) throws IOException {
         return new WorkerImpl(socket, latch, computeServerSettings);
     }
 
--- IndividualThreadServer/src/main/java/ru/ifmo/java/individualThreadServer/WorkerImpl.java
+++ IndividualThreadServer/src/main/java/ru/ifmo/java/individualThreadServer/WorkerImpl.java
@@ -11,6 +11,8 @@ import ru.ifmo.java.commonPartsOfComputeServer.ServerMetrics;
 import ru.ifmo.java.commonPartsOfComputeServer.ServerMetrics4;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,67 +21,79 @@ import java.util.function.Function;
 
 public class WorkerImpl implements Worker {
     private final Socket socket;
+    private final InputStream inputStream;
+    private final OutputStream outputStream;
     private final CountDownLatch latch;
     private final ComputeServerSettings computeServerSettings;
     private volatile AverageServerMetrics averageServerMetrics;
 
-    public WorkerImpl(Socket socket, CountDownLatch latch, ComputeServerSettings computeServerSettings) {
+    public WorkerImpl(Socket socket, CountDownLatch latch, ComputeServerSettings computeServerSettings) throws IOException {
         this.socket = socket;
+        inputStream = socket.getInputStream();
+        outputStream = socket.getOutputStream();
         this.latch = latch;
         this.computeServerSettings = computeServerSettings;
     }
 
     @Override
     public void run() {
-        latch.countDown();
         try {
-            latch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            return;
-        }
-        List<ServerMetrics> serverMetricsList = new ArrayList<>();
-        int index = 0;
-        for (; index < computeServerSettings.getNumberOfRequest() &&
-                !Thread.interrupted() && !socket.isClosed(); ++index) {
-            byte[] bytes;
+            latch.countDown();
             try {
-                bytes = MessageProcessing.readPackedMessage(socket.getInputStream());
-            } catch (IOException e) {
-                break;
-            }
-            List<Double> numberList;
-            try {
-                numberList = MessageWithListOfDoubleVariables.parseFrom(bytes).getNumberList();
-            } catch (InvalidProtocolBufferException e) {
+                latch.await();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
-                break;
+                return;
             }
-            Function<ServerMetrics4, ServerMetrics4> serverMetrics4Initializer = (ServerMetrics4 x) -> {
-                x.setClientProcessingStart(System.currentTimeMillis());
-                x.setRequestProcessingStart(System.currentTimeMillis());
-                return x;
-            };
-            List<Double> list = Sort.sort(numberList);
-            serverMetrics4Initializer = serverMetrics4Initializer.andThen((ServerMetrics4 x) ->
-            {
-                x.setRequestProcessingEnd(System.currentTimeMillis());
-                x.setClientProcessingEnd(System.currentTimeMillis());
-                return x;
-            });
-            byte[] response = MessageWithListOfDoubleVariables.newBuilder().addAllNumber(list).build().toByteArray();
+            List<ServerMetrics> serverMetricsList = new ArrayList<>();
+            int index = 0;
+            for (; index < computeServerSettings.getNumberOfRequest() &&
+                    !Thread.interrupted() && !socket.isClosed(); ++index) {
+                byte[] bytes;
+                try {
+                    bytes = MessageProcessing.readPackedMessage(inputStream);
+                } catch (IOException e) {
+                    break;
+                }
+                List<Double> numberList;
+                try {
+                    numberList = MessageWithListOfDoubleVariables.parseFrom(bytes).getNumberList();
+                } catch (InvalidProtocolBufferException e) {
+                    e.printStackTrace();
+                    break;
+                }
+                Function<ServerMetrics4, ServerMetrics4> serverMetrics4Initializer = (ServerMetrics4 x) -> {
+                    x.setClientProcessingStart(System.currentTimeMillis());
+                    x.setRequestProcessingStart(System.currentTimeMillis());
+                    return x;
+                };
+                List<Double> list = Sort.sort(numberList);
+                serverMetrics4Initializer = serverMetrics4Initializer.andThen((ServerMetrics4 x) ->
+                {
+                    x.setRequestProcessingEnd(System.currentTimeMillis());
+                    x.setClientProcessingEnd(System.currentTimeMillis());
+                    return x;
+                });
+                byte[] response = MessageWithListOfDoubleVariables.newBuilder().addAllNumber(list).build().toByteArray();
+                try {
+                    outputStream.write(MessageProcessing.packMessage(response));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+                serverMetricsList.add(serverMetrics4Initializer.apply(ServerMetrics4.create()));
+            }
+            averageServerMetrics = AverageServerMetrics.create(
+                    serverMetricsList.stream().mapToDouble(ServerMetrics::getRequestProcessingTime).sum(),
+                    serverMetricsList.stream().mapToDouble(ServerMetrics::getClientProcessingTime).sum(),
+                    index);
+        } finally {
             try {
-                socket.getOutputStream().write(MessageProcessing.packMessage(response));
+                socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
-                break;
             }
-            serverMetricsList.add(serverMetrics4Initializer.apply(ServerMetrics4.create()));
         }
-        averageServerMetrics = AverageServerMetrics.create(
-                serverMetricsList.stream().mapToDouble(ServerMetrics::getRequestProcessingTime).sum(),
-                serverMetricsList.stream().mapToDouble(ServerMetrics::getClientProcessingTime).sum(),
-                index);
     }
 
     @Override
